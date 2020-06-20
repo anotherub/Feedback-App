6fecd8ef77e302369547b522dec1c38a0f3da479
umeshbhat <ubhat@mscripts.com>
2020-06-21 00:46:53 +0530

    added auth flow

--- server/index.js
+++ server/index.js
@@ -1,13 +1,34 @@
 const express = require('express')
-const app = express()
+const mongoose = require('mongoose')
+const keys = require('./config/keys')
+const cookieSession = require('cookie-session')
+const passport = require('passport')
 
+require('./models/User')
 require('./services/passport')
-require('./routes/authRoutes')(app)
-app.get('/', (req, res) => {
-  res.send({ hi: 'tehre' })
+
+mongoose.connect(keys.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connection.once('open', () => {
+  console.log('MongoDB database connection established successfully')
 })
+const app = express()
+app.use(cookieSession({ maxAge: 30 * 24 * 60 * 60 * 1000, keys: [keys.cookieKey] }))
+app.use(passport.initialize())
+app.use(passport.session())
+
+app.get('/', (req, res) => {
+  res.send({ hi: 'there' })
+})
+
+require('./routes/authRoutes')(app)
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
   console.log('Server is up and running on port:', PORT)
 })
+
+// mongodb+srv://umeshbhat:umeshbhat@cluster0-24fhy.mongodb.net/feedbackapp-prod?retryWrites=true&w=majority
+
+//970763464240-4us97nd4juna9unk5fcei81oviutd1uj.apps.googleusercontent.com
+
+//H35wSw2o1MmkoRwrSaJnlZIs
--- server/services/passport.js
+++ server/services/passport.js
@@ -1,6 +1,18 @@
 const passport = require('passport')
+const mongoose = require('mongoose')
 const GoogleStrategy = require('passport-google-oauth20').Strategy
 const keys = require('../config/keys')
+const User = mongoose.model('users')
+
+passport.serializeUser((user, done) => {
+  done(null, user.id)
+})
+passport.deserializeUser((id, done) => {
+  User.findById(id).then((doc) => {
+    done(null, doc)
+  })
+})
+
 passport.use(
   new GoogleStrategy(
     {
@@ -9,7 +21,20 @@ passport.use(
       callbackURL: '/auth/google/callback'
     },
     (accessToken, refreshToken, profile, done) => {
-      console.log(accessToken, refreshToken, profile, done)
+      User.findOne({ googleID: profile.id })
+        .then((doc) => {
+          if (doc) {
+            console.log('account already exists')
+            done(null, doc)
+          } else {
+            new User({ googleID: profile.id }).save().then((doc) => {
+              done(null, doc)
+            })
+          }
+        })
+        .catch((err) => {
+          console.log('Error in finding googleID', err)
+        })
     }
   )
 )
