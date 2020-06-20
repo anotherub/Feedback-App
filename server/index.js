4fd818ae961943030e08851261ad409c585909fc
umeshbhat <ubhat@mscripts.com>
2020-06-20 10:43:39 +0530

    added Google auth

--- server/index.js
+++ server/index.js
@@ -1,7 +1,13 @@
 const express = require('express')
 const app = express()
+
+require('./services/passport')
+require('./routes/authRoutes')(app)
 app.get('/', (req, res) => {
-  res.send({ hi: 'there' })
+  res.send({ hi: 'tehre' })
 })
+
 const PORT = process.env.PORT || 5000
-app.listen(PORT)
+app.listen(PORT, () => {
+  console.log('Server is up and running on port:', PORT)
+})
