ca7fbbee08c2cd9b07ea99fec9e8e579df24d099
umeshbhat <ubhat@mscripts.com>
2020-06-24 01:43:18 +0530

    removed / path from epress

--- server/index.js
+++ server/index.js
@@ -19,10 +19,6 @@ app.use(cookieSession({ maxAge: 30 * 24 * 60 * 60 * 1000, keys: [keys.cookieKey]
 app.use(passport.initialize())
 app.use(passport.session())
 
-app.get('/', (req, res) => {
-  res.send({ hi: 'there' })
-})
-
 require('./routes/authRoutes')(app)
 require('./routes/billingRoutes')(app)
 
