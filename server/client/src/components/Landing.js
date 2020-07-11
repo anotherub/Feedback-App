848837246c4f46f99aaef37d1298cb6b3b348236
umeshbhat <ubhat@mscripts.com>
2020-07-11 19:15:36 +0530

    added design for landing page

--- server/client/src/components/Landing.js
+++ server/client/src/components/Landing.js
@@ -1,7 +1,15 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
-function Landing() {
-  return <div style={{ textAlign: 'center' }}>landing</div>
+const Landing = () => {
+  return (
+    <Link to='/surveys' style={{ color: 'black' }}>
+      <div style={{ textAlign: 'center' }}>
+        <h1>FeedBox!</h1>
+        Collect feedback form your users
+      </div>
+    </Link>
+  )
 }
 
 export default Landing
